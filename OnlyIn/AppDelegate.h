b71e4f566d99d1b870c37f5c6873362dd1be47bf
Jennifer Clark <jac300@nyu.edu>
2013-03-25 12:04:35 -0400

    Initial Commit

---
+++ OnlyIn/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  OnlyIn
+//
+//  Created by Jennifer Clark on 3/25/13.
+//  Copyright (c) 2013 Jennifer Clark. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
